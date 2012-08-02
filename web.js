b0564671a47058b0b151d978ee9b8f8591702b6e
Chris Bloom <bloomcb@gmail.com>
2012-08-01 22:58:22 -0700

    update for json output

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express.createServer();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Test 123!');
+});
+
+app.get('/user/:name', function(req, res) {
+	// console.log(response);
+	var user = {"name": req.params.name};
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.write(JSON.stringify(user));
+	res.end();
 });
 
 var port = process.env.PORT || 5000;
